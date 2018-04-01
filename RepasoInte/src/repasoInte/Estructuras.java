d9fc943e2ab3e2d299a4b2aaa4e4b293bfc3f638
Javier210972 <37030402+Javier210972@users.noreply.github.com>
2018-04-01 13:55:44 -0600

    Add files via upload

---
+++ RepasoInte/src/repasoInte/Estructuras.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repasoInte;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author toshiba2
+ */
+public class Estructuras {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//        Para String se usa equals
+//           diferent a---->   !ingreso.equals("f")
+if (true) {
+            
+        }
+//     Scanner A = new Scanner (System.in);
+//        
+//        int a;
+//        a=A.nextInt();
+//        switch(a){
+//            case 2:
+//                System.out.println("si es 2");
+//                break;
+//            default:
+//                System.out.println("no es 0");
+//        }
+
+//while(){
+//
+//}
+
+//Scanner A = new Scanner (System.in);
+//       
+//        String a;
+//       a=A.next();
+//
+// do{
+//     System.out.println("1");
+//    }
+//while(a.equals(1));
+    
+//        for (int i = 0; i < 10; i++) {
+//            
+//        }
+       
+                
+    }
+    
+}
