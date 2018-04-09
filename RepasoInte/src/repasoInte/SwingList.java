2e8b69551e1887d6a6817ee89679ef197089e4e2
Javier210972 <37030402+Javier210972@users.noreply.github.com>
2018-04-08 18:41:52 -0600

    Add files via upload

---
+++ RepasoInte/src/repasoInte/SwingList.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repasoInte;
+//import javax.swing.DefaultListModel;
+//import vista.JFrmFormulario;
+//
+///**
+// *
+// * @author toshiba2
+// */
+//public class ModeloFormulario {
+//
+//    private DefaultListModel lista;
+//    private String texto;
+//    private String caracter;
+//    private int largo;
+//    private int letras;
+//
+//    public ModeloFormulario() {
+//        this.largo = 0;
+//        this.texto = "";
+//        this.caracter = "";
+//        this.letras = 0;
+//        lista = new DefaultListModel();
+//    }
+//
+//    public String getTexto() {
+//        return texto;
+//    }
+//
+//    public String getCaracter1() {
+//
+//        return caracter;
+//    }
+//
+//    public void setCaracter(String caracter) {
+//        this.caracter = caracter;
+//    }
+//
+//    public void setTexto(String texto) {
+//        this.texto = texto;
+//    }
+//
+//    public int getLargo() {
+//        return largo;
+//    }
+//
+//    public void setLargo(int largo) {
+//        this.largo = largo;
+//    }
+//
+//    public DefaultListModel generarTabla1() {
+//        lista.clear();
+//        for (int i = 1; i <= texto.length(); i++) {
+//            caracter = texto.substring(i - 1, i);
+//            lista.addElement(caracter);
+//
+//        }
+//
+//        return lista;
+//    }
